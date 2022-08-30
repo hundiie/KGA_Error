f9b99f17ad8b5c6538ec9a794df9e92b3fe4f001
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-08-30 17:44:58 +0900

    Oculus와 연동

---
+++ KGA_Error/Assets/01_Scripts/Buttonss.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Buttonss : MonoBehaviour
+{
+    public enum ButtonInfo  // 버튼 종류
+    {
+        OneButton,
+        TwoButton_L,
+        TwoButton_R,
+    };
+    public ButtonInfo ButtonState;
+
+    public bool IsPush = false; // 눌렸는지 체크
+
+    // 외부 스크립트
+    private RoomController roomController;
+    private ButtonController buttonController;
+
+    // 애니메이션
+    private Animator buttonAmim;    // 버튼 누르는 애니메이션
+    private float buttonBlend;      // 애니메이션 블랜드값
+    
+    // 버튼 텍스트
+    private TextMeshPro ButtonTMP;  // 버튼 바로 위에 뜨는 텍스트
+    private string ButtonText;      // CSV에서 가져오는 텍스트
+
+    private float timer;    // 플레이어 주시 관련
+
+    private void Awake()
+    {
+        roomController = GetComponentInParent<RoomController>();
+        buttonController = GetComponentInParent<ButtonController>();
+        buttonAmim = GetComponent<Animator>();
+        ButtonTMP = GetComponentInChildren<TextMeshPro>();
+    }
+
+    private void Start()
+    {
+        // 버튼 텍스트 파싱
+        switch ((int)ButtonState)
+        {
+            case 0: // 한개버튼
+                ButtonText = CSVParser.Instance.GetCsvOneButton((int)roomController.roomInfo);
+                buttonController.checkingText.enabled = false;
+                break;
+            case 1: // 왼쪽버튼
+                ButtonText = CSVParser.Instance.GetCsvTwoButton_L((int)roomController.roomInfo);
+                break;
+            case 2: // 오른쪽 버튼
+                ButtonText = CSVParser.Instance.GetCsvTwoButton_R((int)roomController.roomInfo);
+                break;
+        }
+        ButtonTMP.text = ButtonText;
+        ButtonTMP.enabled = false;
+
+        buttonBlend = 0f;
+    }
+    private void Update()
+    {
+        if (IsPush)
+        {
+            StartCoroutine("PushDown");
+            float blendSpeed = Time.deltaTime * 1.5f;
+            buttonBlend += blendSpeed; // 블랜드값 0 -> 1
+        }
+
+        // 플레이어 주시에 따른 버튼 텍스트 관련
+        timer += Time.deltaTime;
+        if (timer >= 0.5f)
+        {
+            ButtonTextDisable();
+            timer = 0f;
+        }
+    }
+
+    /// <summary>
+    /// 눌리는 애니메이션과 checking 텍스트 노출
+    /// </summary>
+    /// <returns>1초</returns>
+    private IEnumerator PushDown()
+    {
+        buttonController.doNotPush = true;
+        buttonAmim.SetFloat("Blend", buttonBlend);
+
+        // 버튼 하나일 때만 checking텍스트 노출
+        if (ButtonState == 0) { buttonController.checkingText.enabled = true; }
+
+        if (buttonBlend >= 1)
+        {
+            IsPush = false; // 애니매이션이 끝나면 눌렸음 상태 변경
+            buttonBlend = 0f;
+
+            yield return null;
+            buttonController.ButtonSoundPlay();
+
+            yield return new WaitForSeconds(3f);
+            if (ButtonState == 0)
+            {
+                GameManager.Instance.CurrentScene++;
+                GameManager.Instance.UpdateScene();
+            }
+            else
+            {
+                GameManager.Instance.TurnIndex++;
+                roomController.RoomTurnUpdate();
+            }
+            StopCoroutine("PushDown");
+        }
+    }
+
+    public void ButtonTextEnable()
+    {
+        ButtonTMP.enabled = true;
+    }
+    public void ButtonTextDisable()
+    {
+        ButtonTMP.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider _other)
+    {
+        if(_other.tag == "OculusHand")
+        {
+            IsPush = true;
+        }
+    }
+}
--- KGA_Error/Assets/01_Scripts/Utill/PlayerInput.cs
+++ KGA_Error/Assets/01_Scripts/Utill/PlayerInput.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using OVR;
+
 public class PlayerInput : MonoBehaviour
 {
     private float xRotate = 0.0f; // 내부 사용할 X축 회전량은 별도 정의 ( 카메라 위 아래 방향 )
@@ -22,8 +24,10 @@ public class PlayerInput : MonoBehaviour
     /// </summary>
     void MouseRotation()
     {
-        float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
-        float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
+        float yRotateSize = 0;
+        //float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
+        float xRotateSize = 0;
+        //float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
         float yRotate = transform.eulerAngles.y + yRotateSize;
         xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);  // 회전량 제한(X축, 하늘방향, 바닥방향)
 
@@ -35,18 +39,21 @@ public class PlayerInput : MonoBehaviour
     /// </summary>
     void KeyboardMove()
     {
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector2 mov2d = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        Vector3 dir = new Vector3(mov2d.x, 0, mov2d.y).normalized;
+
+        // Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         dir = Camera.main.transform.TransformDirection(dir);
         dir.y = 0f;
         transform.position += (dir * moveSpeed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            moveSpeed *= 2f;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            moveSpeed /= 2f;
-        }
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    moveSpeed *= 2f;
+        //}
+        //if (Input.GetKeyUp(KeyCode.LeftShift))
+        //{
+        //    moveSpeed /= 2f;
+        //}
     }
 
     /// <summary>
@@ -55,17 +62,22 @@ public class PlayerInput : MonoBehaviour
     void Raycast()
     {
         RaycastHit hit; // 충돌정보
-        float maxDistance = 20f; // 검사 최대 거리
+        float maxDistance = 25f; // 검사 최대 거리
 
         if (Physics.Raycast(CameraTransform.position, CameraTransform.forward, out hit, maxDistance))
         {
             if (hit.transform.tag == "Button") // 버튼 감지하면
             {
-                hit.transform.GetComponent<Button>()?.ButtonTextEnable(); // 버튼텍스트 출력
+                hit.transform.GetComponent<Buttonss>()?.ButtonTextEnable(); // 버튼텍스트 출력
                 if (Input.GetMouseButton(0) && !hit.transform.GetComponentInParent<ButtonController>().doNotPush) // 클릭하면
                 {
-                    hit.transform.GetComponent<Button>().IsPush = true; // 눌렀다고 알려주기
+                    hit.transform.GetComponent<Buttonss>().IsPush = true; // 눌렀다고 알려주기
                 }
+                else if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && !hit.transform.GetComponentInParent<ButtonController>().doNotPush) // 클릭하면
+                {
+                    hit.transform.GetComponent<Buttonss>().IsPush = true; // 눌렀다고 알려주기
+                }
+                
             }
         }
     }
